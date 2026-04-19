b3f1e57d432ebdea6409b3a0a108865d1adb6489
Nilesh Raj <rajnilesh088@gmail.com>
2026-04-19 10:45:29 +0530

    Initial commit

---
+++ src/main/java/com/nilesh/DDED/DomesticDataExchangeDemoApplication.java
@@ -0,0 +1,14 @@
+package com.nilesh.DDED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomesticDataExchangeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomesticDataExchangeDemoApplication.class, args);
+		System.out.println("Welcome to Spring boot Cloud Config Solution");
+	}
+
+}
---
+++ src/test/java/com/nilesh/DDED/DomesticDataExchangeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilesh.DDED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticDataExchangeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
